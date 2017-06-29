6b55eaa46bb96e5b6c091c7a5094278fd6e90211
Angela Xu <angela.xu@softwire.com>
2017-06-29 21:43:37 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,14 @@
+var viewModel = {
+    family: [
+        {
+            name: 'Andrew',
+            description: 'Hmm. -_-'
+        },
+        {
+            name: 'Another person',
+            description: 'This is a proof of concept and I can\'t be bothered to go through the whole family right now'
+        }
+    ]
+};
+
+ko.applyBindings(viewModel);
